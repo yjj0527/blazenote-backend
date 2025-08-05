810e2fa82982cc40399bea01d1ba6718dba9bc80
yjj0527 <yjj_yangjianji@sina.com>
2025-08-05 14:33:59 +0800

    Implement CRUD operations with D1 database

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ const app = new Hono();
 // Important! No trailing slash
 const allowedOrigins = new Set([
   "http://localhost:5173",
-  "https://<your-frontend-domain>.<tld>",
+  "https://blazenote-frontend-6th.pages.dev/",
 ]);
 
 // https://hono.dev/docs/middleware/builtin/cors
